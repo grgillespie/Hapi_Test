b9a20ea8b26dff8ad629e8fe5cfa17bc037838ab
grgillespie <grgillespie88@gmail.com>
2023-11-08 11:16:53 -0700

    Update simulation_test.py

--- simulation_test.py
+++ simulation_test.py
@@ -7,3 +7,10 @@ Created on Wed Nov  8 10:05:29 2023
 
 #beep boop
 #boop
+
+import sys
+sys.path.append(r"C:\Users\grgil\OneDrive - UCB-O365\CU Boulder\Code\Test v2\hapi.py")
+import hapi
+
+import matplotlib.pyplot as plt
+
